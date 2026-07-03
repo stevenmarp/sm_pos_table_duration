6f7d44a85117c4aadd7cc2a8c558725cf42dcc73
stevenmarp <100309799+stevenmarp@users.noreply.github.com>
2026-07-03 19:56:15 +0700

    chore: add app store banner and index.html tweaks

--- sm_pos_table_duration/__manifest__.py
+++ sm_pos_table_duration/__manifest__.py
@@ -19,6 +19,7 @@ restaurant floor plan.
     "author": "Steven Marp",
     "website": "https://apps.odoo.com/apps/modules/browse?author=Steven Marp",
     "license": "OPL-1",
+    "images": ["static/description/banner.gif"],
     "depends": ["pos_restaurant"],
     "assets": {
         "point_of_sale._assets_pos": [
